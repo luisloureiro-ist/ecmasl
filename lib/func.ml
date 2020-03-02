b105c7bdad6a5e598766287377053b7183a6921e
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-03-02 02:08:49 +0000

    Create "str" function in Func module.

--- lib/func.ml
+++ lib/func.ml
@@ -9,3 +9,5 @@ let create_store (func : t) (vals : Val.t list) : Store.t =
   Store.create_store varvals
 
 let get_body (func : t) : Stmt.t = func.body
+
+let str (func : t) : string = "function " ^ func.name ^ " (" ^ List.fold_left (fun acc ele -> acc ^ ele) ", " func.params ^ ") { " ^ Stmt.str func.body
