5a81ab24d6463d2636dc876a718ad38728462688
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-03-02 02:06:49 +0000

    Create to_string function to be able to print "prog" state;

--- lib/prog.ml
+++ lib/prog.ml
@@ -1,3 +1,5 @@
 type t = (string, Func.t) Hashtbl.t
 
 let get_func (prog : t) (func : string) : Func.t = Hashtbl.find prog func
+
+let to_string (prog : t) : string = Hashtbl.fold (fun n v ac -> (if ac <> "" then ac ^ "\n" else ac) ^ (Printf.sprintf "(%s -> %s)" n (Func.str v))) prog ""
