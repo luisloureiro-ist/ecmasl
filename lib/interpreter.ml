65be7ff99792a5aea1dad9f18121e0c48667ed9f
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-03-05 18:04:19 +0000

    Refactor: - move the result of the "Call" pattern to a different function;

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -17,15 +17,8 @@ let rec eval_expr (prog : Prog.t) (sto : Store.t) (e : Expr.t) : Val.t = match e
   | Var x                -> Store.get_var sto x
   | UnOpt (uop, e)       -> let v = eval_expr prog sto e in eval_unopt_expr uop v
   | BinOpt (bop, e1, e2) -> let v1 = eval_expr prog sto e1 and v2 = eval_expr prog sto e2 in eval_binopt_expr bop v1 v2
-  | Call (f, es)         -> (let vs = List.map (eval_expr prog sto) es in
-                             let func = Prog.get_func prog f in
-                             let sto = Func.create_store func vs in
-                             let s = Func.get_body func in
-                             let v0 = eval_stmt prog sto s in
-                             match v0 with
-                               None -> raise (Invalid_argument "Exception in Interpreter.eval_expr: statement didn't return")
-                             | Some v -> v
-                            )
+  | Call (f, es)         -> let vs = List.map (eval_expr prog sto) es in fst (eval_proc prog f vs)
+
 (* Syntax for mutually recursive functions *)
 and eval_stmt (prog : Prog.t) (sto: Store.t) (s: Stmt.t) : Val.t option = match s with
     Skip           -> None
@@ -37,3 +30,12 @@ and eval_stmt (prog : Prog.t) (sto: Store.t) (s: Stmt.t) : Val.t option = match 
   | If (e, s1, s2) -> let v = eval_expr prog sto e in if (Val.is_true v) then eval_stmt prog sto s1 else eval_stmt prog sto s2
   | While (e, s)   -> eval_stmt prog sto (If (e, Seq (s, While (e, s)), Skip))
   | Return exp     -> let v = eval_expr prog sto exp in Some v
+
+and eval_proc (prog : Prog.t) (pname : string) (args : Val.t list) : Val.t * Store.t =
+  let func = Prog.get_func prog pname in
+  let sto = Func.create_store func args in
+  let s = Func.get_body func in
+  let v0 = eval_stmt prog sto s in
+  match v0 with
+    None -> raise (Invalid_argument "Exception in Interpreter.eval_proc: statement didn't return")
+  | Some v -> (v, sto)
