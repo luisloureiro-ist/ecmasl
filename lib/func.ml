0b741141256378a3b2703d778f5cacf2e1b6605c
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-03-09 17:20:13 +0000

    Add get functions for the other "Func" type fields;

--- lib/func.ml
+++ lib/func.ml
@@ -10,6 +10,8 @@ let create_store (func : t) (vals : Val.t list) : Store.t =
 
 let create (name : string) (params : string list) (body : Stmt.t) : t = { name; params; body }
 
+let get_name (func : t) : string = func.name
+let get_params (func : t) : string list = func.params
 let get_body (func : t) : Stmt.t = func.body
 
 let str (func : t) : string = "function " ^ func.name ^ " (" ^ List.fold_left (fun acc ele -> acc ^ ele) ", " func.params ^ ") { " ^ Stmt.str func.body
