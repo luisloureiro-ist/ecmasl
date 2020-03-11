d8510925faa1594c344eb202f10bbb9fac2cff94
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-03-11 10:35:55 +0000

    Add missing function to the heap module;

--- lib/heap.ml
+++ lib/heap.ml
@@ -11,4 +11,23 @@ let update (heap : t) (loc : Loc.t) (obj : Object.t) : unit = Hashtbl.replace he
 
 let get (heap : t) (loc : Loc.t) : Object.t option = Hashtbl.find_opt heap loc
 
+let get_field (heap : t) (loc : Loc.t) (field : Field.t) : Val.t option =
+  let obj = get heap loc in
+  let v = match obj with
+    | None   -> None
+    | Some o -> Object.get o field in
+  v
+
+let set_field (heap : t) (loc : Loc.t) (field : Field.t) (value : Val.t) : unit =
+  let obj = get heap loc in
+  match obj with
+  | None   -> ()
+  | Some o -> Object.set o field value
+
+let delete_field (heap : t) (loc : Loc.t) (field : Field.t) : unit =
+  let obj = get heap loc in
+  match obj with
+  | None   -> ()
+  | Some o -> Object.delete o field
+
 let str (heap : t) : string = (Hashtbl.fold (fun n v ac -> (if ac <> "" then ac ^ ", " else ac) ^ (Printf.sprintf "%s: %s" (Loc.str n) (Object.str v))) heap "{ ") ^ " }"
